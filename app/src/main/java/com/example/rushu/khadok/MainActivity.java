382d844a1fb08a79cf796ae5d06190bcec270c3e
Rushu29 <fazlerabbirushu29@gmail.com>
2017-03-10 14:19:09 +0600

    Initial commit

---
+++ app/src/main/java/com/example/rushu/khadok/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rushu.khadok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
